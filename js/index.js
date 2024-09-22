bb50e0fc3c81a772c0d8da727bcf51f26801595b
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2024-09-22 19:46:30 +0600

    Full Project Done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Global declared function
 function getElementValueById(id) {
   const inputValue = parseFloat(document.getElementById(id).value);
   return inputValue;
@@ -11,6 +12,7 @@ function getElementInnerTextById(id) {
   return elementInnerText;
 }
 
+// Global declared function for all value
 function allInputField() {
   const income = getElementValueById("income");
   const software = getElementValueById("software");
@@ -32,6 +34,7 @@ function allInputField() {
   };
 }
 
+// Calculate button click function
 document.getElementById("calculate").addEventListener("click", function () {
   const {
     income,
@@ -49,7 +52,25 @@ document.getElementById("calculate").addEventListener("click", function () {
   const calculate = income - totalExpenses;
   balance.innerText = calculate;
   document.getElementById("results").classList.remove("hidden");
+
+  // History Added
+  const history = document.createElement("div");
+
+  history.classList.add("border-l-2", "p-2", "border-purple-600", "rounded-xl");
+  history.innerHTML = `
+  <p class="text-gray-500 text-sm ">Time: ${new Date().toLocaleTimeString()}</p>
+  <p class="text-gray-500 text-sm ">Date:  ${new Date().toLocaleDateString()}</p>
+  <p class="text-gray-500 text-sm ">Income: ${income.toFixed(2)}</p>
+  <p class="text-gray-500 text-sm ">Expenses: ${totalExpenses.toFixed(2)}</p>
+  <p class="text-gray-500 text-sm ">Balance: ${calculate.toFixed(2)}</p>
+
+`;
+
+  // Append the new history element to the history list
+  document.getElementById("history-list").appendChild(history);
 });
+
+// Calculate savings button click function
 
 document
   .getElementById("calculate-savings")
@@ -73,3 +94,39 @@ document
     savings.innerText = totalSavings;
     remainingBalance.innerText = remainBalance;
   });
+
+// Assistant button click function
+document.getElementById("assistant-tab").addEventListener("click", function () {
+  getElementsById("assistant-tab").classList.add(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElementsById("history-tab").classList.remove(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElementsById("expense-form").classList.remove("hidden");
+  getElementsById("history-section").classList.add("hidden");
+});
+
+// History button click function
+document.getElementById("history-tab").addEventListener("click", function () {
+  getElementsById("history-tab").classList.add(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElementsById("assistant-tab").classList.remove(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElementsById("expense-form").classList.add("hidden");
+  getElementsById("history-section").classList.remove("hidden");
+});
